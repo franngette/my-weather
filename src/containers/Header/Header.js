959a93045e08600ca807dbc3ffb1257955dab405
Francis Gette <franngette@gmail.com>
2021-04-18 23:36:00 -0300

    added header styles

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './Header.scss'
 const Header = () => {
     return (
-        <header className='header'>My Weather</header>
+        <header className='header'>
+            <h1 className='header__title'>My Weather</h1>
+        </header>
     );
 }
 
